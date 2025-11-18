186e06ca54e05a30f9a6b925d22704ec4518d303
omerr0542 <o.faruk0542@gmail.com>
2025-11-18 23:47:33 +0300

    initial

--- AITech.DataAccess/Repositories/GenericRepositories/IRepository.cs
+++ AITech.DataAccess/Repositories/GenericRepositories/IRepository.cs
@@ -5,7 +5,7 @@ namespace AITech.DataAccess.Repositories.GenericRepositories;
 public interface IRepository<TEntity> where TEntity : BaseEntity
 {
     Task<List<TEntity>> GetAllAsync();
-    Task<TEntity> GetByIdAsync(int id);
+    Task<TEntity> GetByIdAsync(int id); 
     Task CreateAsync(TEntity entity);
     Task UpdateAsync(TEntity entity);
     Task DeleteAsync(int id);
